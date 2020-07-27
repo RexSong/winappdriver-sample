380de5055c46a6dcaf483c5c6f11a90088f2d1a6
Rex.Song <westerncampus@gmail.com>
2020-07-27 19:47:43 +0800

    remove unused tests

--- src/test/java/com/rex/test/uwp/test/winappdriversample/WinappdriverSampleApplicationTests.java
+++ src/test/java/com/rex/test/uwp/test/winappdriversample/WinappdriverSampleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.rex.test.uwp.test.winappdriversample;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WinappdriverSampleApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.rex.test.uwp.test.winappdriversample;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WinappdriverSampleApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
